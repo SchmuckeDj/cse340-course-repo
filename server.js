d48551a15cd10073047fac44d7b5285e02ea2189
SchmuckeDj <schjosepu@gmail.com>
2026-09-01 13:14:31 -0400

    first term

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+app.get('/organizations', (req, res) => {
+    res.render('organizations', { title: 'Organizations' });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
